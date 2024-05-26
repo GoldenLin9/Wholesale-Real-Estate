49387396e5c0b99355b3de73008d4ff5c8b5f109
GoldenLin9 <goldenlin0909@gmail.com>
2024-05-25 22:12:30 -0400

    new pull and moves old files to depracated

--- public_records/get_probates.py
+++ public_records/get_probates.py
@@ -1,8 +1,8 @@
-from public_records.get_CSV import *
+from get_CSV import *
 import time
 
 driver = setup()
-change_date(driver, selection = "Last Month")
+change_date(driver, selection = "Last 14 Days")
 change_doc_type(driver, selection = "PROBATE DOCUMENT")
 download_CSV(driver)
 
