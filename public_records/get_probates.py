1fe030c0df023b290afbd507b23a0918c9094d18
GoldenLin9 <goldenlin0909@gmail.com>
2024-05-27 15:59:03 -0400

    tried to revert to orig?

--- public_records/get_probates.py
+++ public_records/get_probates.py
@@ -1,8 +1,8 @@
-from get_CSV import *
+from public_records.get_CSV import *
 import time
 
 driver = setup()
-change_date(driver, selection = "Last 14 Days")
+change_date(driver, selection = "Last Month")
 change_doc_type(driver, selection = "PROBATE DOCUMENT")
 download_CSV(driver)
 
